5fa52f7474ba4a90065a7696abbfe0c2f5fe1716
Jr.Soares <juniorsoaresti@gmail.com>
2022-01-25 11:42:26 -0400

    Adding input select and button on Dashboard page

--- src/components/EndomarketingList/index.js
+++ src/components/EndomarketingList/index.js
@@ -5,21 +5,20 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import { MoreHoriz } from '@mui/icons-material';
 
 
 export function EndomarketingList() {
   return (
     <Card sx={{ width: '552', marginBottom: "10px" }}>
       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: "8px 11px" }}>
-        <Box component="img" src="./assets/images/1.png" sx={{ width: '73px', height: '73px' }}></Box>
+        <img src="./assets/images/1.png" alt="" />
         <CardContent>
-          <Typography variant="h1" sx={{
-            fontWeight: '700', fontSize: '16px', color: "#707070", marginBottom: '5px'
+          <Typography sx={{
+            fontWeight: '700', fontSize: '16px', color: "#707070", marginBottom: '5px',
           }} >
             Geral dispensado para curtir o feriado
           </Typography>
-
           <Box sx={{
             display: 'flex',
           }}>
@@ -27,22 +26,30 @@ export function EndomarketingList() {
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              backgroundColor: '#3489B1', width: '48px', height: '11px', fontSize: '6px', fontWeight: '700', color: "#eee", marginRight: '5px'
+              backgroundColor: '#3489B1', width: '50px', height: '15px', fontSize: '6px', fontWeight: '700', color: "#eee", marginRight: '5px', fontFamily: "Open Sans"
             }} >
               COMUNICADO
             </Typography>
-            <Typography sx={{ fontSize: '9px' }} color="#707070">
+            <Typography sx={{ fontSize: '9px', fontFamily: "Open Sans", color: "#707070" }}>
               13 DE OUT
             </Typography>
           </Box>
-          <Typography variant="body2">
+          <Typography sx={{
+            color: "#707070",
+            fontWeight: "300",
+            fontFamily: "Open Sans",
+            fontSize: "13px",
+            marginTop: "10px",
+          }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt...
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">More</Button>
+          <Button size="small">
+            <MoreHoriz sx={{ color: "#707070" }} />
+          </Button>
         </CardActions>
-      </Box>
+      </Box >
     </Card >
   );
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,7 @@
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { Typography, Box, FormControl, Select, MenuItem, Button } from '@mui/material'
 import Header from '../../components/Header'
 import { EndomarketingList } from '../../components/EndomarketingList'
-
-
+import AddIcon from '@mui/icons-material/Add';
 export default function Dashboard() {
   return (
     <>
@@ -16,13 +14,48 @@ export default function Dashboard() {
           marginLeft: "78px",
           width: "810px",
         }}>
-          <Typography>
-            <h1>Endomarketing</h1>
-          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+            <Typography sx={{
+              color: "#707070",
+              fontWeight: "300",
+              fontFamily: "Open Sans",
+              fontSize: "35px",
+
+            }}>
+              <h1>Endomarketing</h1>
+            </Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }} s>
+              <FormControl sx={{ m: 1 }}>
+                <Select
+                  sx={{ width: "95px", height: "38px", backgroundColor: "#fff" }}
+                  value={{}}
+                  onChange={{}}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                >
+                  <MenuItem value="">
+                    <em>TIPO</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <Button variant="contained" sx={{
+                  backgroundColor: "#3489B1", width: "103px",
+                  height: "38px",
+                }}>Criar <AddIcon sx={{
+                  marginLeft: "10px",
+                  fontSize: "18px"
+                }} />
+                </Button>
+              </FormControl>
+            </Box>
+          </Box>
           <EndomarketingList />
         </Box>
       </Box>
-
     </>
   )
 }
